04b78617db13d7a2fbca992a47cbe5fc9c9711b2
omerbe <omerbe@nymhealth.com>
2022-10-21 19:21:16 +0300

    create secrets.py

---
+++ secrets.py
@@ -0,0 +1,14 @@
+# API
+URL = 'https://api.football-data.org/v2'
+URL_COMPETITION_MATCHES = f'{URL}/competitions/WC/matches'
+
+# API TOKEN
+HEADERS = {'X-Auth-Token': 'a5b3683eec044716a6c0730cb9c56917'}
+
+# TELEGRAM BOT TOKEN
+prod_token = "1804390138:AAF3qeSyYFBcjHyDEGif3UbXAP5fCTCSzUw"
+test_token = "5469557083:AAGqa0y7ZubAtlQyi2IULHOUmWzmGYqulK8"
+
+# DB NAMES
+BETS_DB = "bets_bot.db"
+BETS_TESTS_DB = "bets_bot_tests.db"
